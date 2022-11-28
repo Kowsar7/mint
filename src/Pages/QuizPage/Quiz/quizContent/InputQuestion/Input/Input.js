4c83eb1769697751c5a7009d372f19ca15d4938f
Kowsar7 <kowsar.mojtabaee@gmail.com>
2022-11-28 17:21:56 +0330

    Input mode

--- src/Pages/QuizPage/Quiz/quizContent/InputQuestion/Input/Input.js
+++ src/Pages/QuizPage/Quiz/quizContent/InputQuestion/Input/Input.js
@@ -4,6 +4,39 @@ import { connect } from "react-redux";
 import classes from "./Input.module.css";
 import * as actionTypes from "../../../../../../redux/actions/actionTypes";
 
+function toEnglishNumber(strNum) {
+  var pn = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+  var en = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var an = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
+  var cache = strNum;
+  for (var i = 0; i < 10; i++) {
+    var regex_fa = new RegExp(pn[i], "g");
+    var regex_ar = new RegExp(an[i], "g");
+    cache = cache.replace(regex_fa, en[i]);
+    cache = cache.replace(regex_ar, en[i]);
+  }
+  return cache;
+}
+
+function validate(event) {
+  var theEvent = event || window.event;
+
+  if (theEvent.type === "paste") {
+    var key = event.clipboardData.getData("text/plain");
+  } else {
+    key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
+  }
+
+  key = toEnglishNumber(key);
+
+  var regex = /[0-9]|\./;
+  if (!regex.test(key)) {
+    theEvent.returnValue = false;
+    if (theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
 const input = (props) => {
   return (
     <div className={classes.Inputs}>
@@ -11,11 +44,16 @@ const input = (props) => {
         <div className={classes.Root}>
           <input
             className={classes.Input}
-            // type="number"
-            // inputMode="numeric"
+            type="text"
+            onKeyPress={(event) => validate(event)}
+            inputMode="numeric"
             value={props.value}
             onChange={(event) =>
-              props.onInputChange(event.target.value, props.scale, props.HorW)
+              props.onInputChange(
+                toEnglishNumber(event.target.value),
+                props.scale,
+                props.HorW
+              )
             }
           />
           <span className={classes.Postfix}>{props.scale}</span>
